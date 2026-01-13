77f5f9fa2a655dd45e93eb26d12d8aab15f27130
Senuthmd <mdsenuthdinsara@gmail.com>
2026-01-13 11:30:11 +0530

    Delete app directory

--- app/profile/page.js
+++
@@ -1,9 +0,0 @@
-export default function Profile() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold">Contact & Info</h1>
-      <p>WhatsApp: +94 743 903 048</p>
-      <p>Email: senumodz@gmail.com</p>
-    </div>
-  );
-}
