b784881df16cc74994ffa3ce47e3952eb83a1f4e
Senuthmd <mdsenuthdinsara@gmail.com>
2026-01-13 11:30:20 +0530

    Delete components directory

--- components/Card.js
+++
@@ -1,8 +0,0 @@
-export default function Card({ title, value }) {
-  return (
-    <div className="bg-gray-900 p-6 rounded shadow-md">
-      <h2 className="font-bold text-lg">{title}</h2>
-      <p className="text-2xl mt-2">{value}</p>
-    </div>
-  );
-}
